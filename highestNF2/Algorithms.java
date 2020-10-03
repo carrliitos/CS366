10fa3d20c3f240cfd72c96600fe3ad21e1bae8c3
carrliitos <salazar.ten80@gmail.com>
2020-10-03 13:39:07 -0500

    checks for 3NF

--- highestNF2/Algorithms.java
+++ highestNF2/Algorithms.java
@@ -76,7 +76,7 @@ public class Algorithms {
 		int ret = not2NF.size();
 		if(ret != 0) {
 			System.out.printf("The highest normal form that the relation satisfies is 1NF\n");
-			System.out.printf("Decomposing to make it 2NF");
+			System.out.printf("Decomposing to 2NF\n");
 		}else return true;
 
 		List<Relation> drel = new ArrayList<>();
@@ -114,7 +114,70 @@ public class Algorithms {
 			r.primaryKey = r.allKeyCandidates.get(0);
 			System.out.println(r);
 		}
-		
+
+		return false;
+	}
+
+	public boolean isIn3NF(Relation rel) {
+		List<FD> fd3NF = rel.fds;
+		List<FD> not3NF = new ArrayList<>();
+		List<Relation> drel = new ArrayList<>();
+		Relation relc = rel.clone();
+		int keyMask = 0;
+
+		for(AttributeSet k : rel.allKeyCandidates) {
+			keyMask = keyMask | k.attMask;
+		}
+
+		for(FD f : fd3NF) {
+			if(rel.allKeyCandidates.contains(f.getLHS())) continue;
+			if((keyMask & f.getRHS().attMask) != 0) continue;
+			else {
+				relc.fds.remove(f);
+				AttributeSet x = f.getRHS().clone();
+				relc.attributes.removeAttSet(x);
+				not3NF.add(f);
+			}
+		}
+
+		if(not3NF.size() != 0) {
+			System.out.printf("The highest normal form that the relation satisifes is 2NF.\n");
+			System.out.printf("Decomposing to 3NF.\n");
+		}else return true;
+
+		List<FD> not2NFb = new ArrayList<>();
+		not2NFb.addAll(not3NF);
+
+		int m = 0; 
+		for(FD f : not2NFb) {
+			m = (m | f.getRHS().attMask);
+		}
+		for(FD f : fd3NF) {
+			if((f.getRHS().attMask & m) != 0) {
+				if(relc.fds.contains(f)) relc.fds.remove(f);
+			}
+		}
+
+		drel.add(relc);
+		for(FD f : not3NF) {
+			Relation r = new Relation();
+			AttributeSet x = f.getLHS().clone();
+			x.union(f.getRHS());
+			r.setAttributes(x);
+
+			List<FD> fdsm = new ArrayList<>();
+			fdsm.add(fdsm);
+			r.setFDs(fdsm);
+			drel.add(r);
+		}
+
+		for(Relation r : drel) {
+			List<AttributeSet> allKeyCandidatesm;
+			allKeyCandidatesm = Algorithms.findAllCandidateKeys(r.fds, r.attributes);
+			r.setKeyCandidates(allKeyCandidatesm);
+			r.primaryKey = r.allKeyCandidates.get(0);
+			System.out.printlf(r);
+		}
 		return false;
 	}
 }
