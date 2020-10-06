b3cd5759581a145d7553e33376e9e2aa0069aab5
carrliitos <salazar.ten80@gmail.com>
2020-10-06 10:55:24 -0500

    updates

---
+++ SQLLesson/CreateTable.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Student(snum BIGINT,
+				sname VARCHAR(50),
+				major VARCHAR(50),
+				level CHAR(2),
+				age int,
+				PRIMARY KEY (snum));
+
+CREATE TABLE ENROLL(snum BIGINT,
+				cname VARCHAR(255),
+				FOREIGN KEY (snum) REFERENCES Student(snum),
+				FOREIGN KEY (cname) REFERENCES Class(cname),
+				PRIMARY KEY (snum, cname));
+
+CREATE TABLE Class(cname VARCHAR(255),
+				meets-at VARCHAR(255),
+				room VARCHAR(255),
+				fid int,
+				PRIMARY KEY (cname));
