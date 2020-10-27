54fc826ee11fab6e237183a03264a6d6b04d6535
carrliitos <salazar.ten80@gmail.com>
2020-10-27 09:22:10 -0500

    organized more programs

--- AttributeClosure/ClosureOfAttributes.java
+++ AttributeClosure/ClosureOfAttributes.java
@@ -1,20 +1,5 @@
 /* @author Benzon Carlitos Salazar CS366 - DBMS
 *	This programs finds attribute closures.
-*	Usage: java ClosureOfAttributes <dependency text file> <closure set you want>
-*	Dependency text file will have the first line with all the attributes, then the
-*		FDs are the lines with a space between the left-hand side and right-hand side
-*	Closure set is whatever closure set you are looking for
-*
-*	Example: Given the relation R(ABCDE) with FDs {A->B, CD->E, AC->D}
-*		DependencyFile.txt will have
-*			ABCDE
-*			A B
-*			CD E
-*			AC D
-*		Find the closure of (AC)+
-*
-*	run: 	java ClosureOfAttributes DependencyFile.txt AC
-*	output: (AC)+ = {ABCDE}
 */
 
 import java.io.File;
---
+++ AttributeClosure/README.md
@@ -0,0 +1,21 @@
+# Attribute Closure
+
+## Usage
+```
+$ java ClosureOfAttributes <dependency text file> <closure set you want>
+```
+- Dependency text file will have the first line with all the attributes, then the FDs are the lines with a space between the left-hand side and right-hand side
+- Closure set is whatever closure set you are looking for
+
+## Example
+Example: Given the relation R(ABCDE) with FDs {A->B, CD->E, AC->D}
+		dependencyfile.txt will have
+			ABCDE
+			A B
+			CD E
+			AC D
+		Find the closure of (AC)+
+```
+$ java ClosureOfAttributes dependencyfile.txt AC
+$ (AC)+ = {ABCDE}
+```
